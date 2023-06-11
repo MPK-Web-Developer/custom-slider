866a963e64d472fd6b98ea1226225288af90c575
Praveen Kumar <m.praveenkumar.github@gmail.com>
2023-06-11 22:01:56 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let customSlideItem = document.querySelectorAll(
+	".custom-slider .custom-slider-item"
+);
+let itemSlideBtn = document.querySelectorAll(
+	".custom-slider .custom-slider-item--nav .custom-nav-btn"
+);
+let currentSlide = 1;
+
+// Slider click slide
+const customManualNav = function (item) {
+	customSlideItem.forEach((slide) => {
+		slide.classList.remove("custom-slide-active");
+
+		itemSlideBtn.forEach((btn) => {
+			btn.classList.remove("custom-slide-active");
+		});
+	});
+
+	customSlideItem[item].classList.add("custom-slide-active");
+	itemSlideBtn[item].classList.add("custom-slide-active");
+};
+
+itemSlideBtn.forEach((btn, i) => {
+	btn.addEventListener("click", () => {
+		customManualNav(i);
+		currentSlide = i;
+	});
+});
+
+// auto Slider
+let customAutoSlide = function (activeClass) {
+	let active = document.getElementsByClassName("custom-slide-active");
+	let iteration = 1;
+	let activeAutoSlide = () => {
+		setTimeout(function () {
+			[...active].forEach((itemSlide) => {
+				itemSlide.classList.remove("custom-slide-active");
+			});
+
+			customSlideItem[iteration].classList.add("custom-slide-active");
+			itemSlideBtn[iteration].classList.add("custom-slide-active");
+			iteration++;
+
+			if (customSlideItem.length == iteration) {
+				iteration = 0;
+			}
+			if (iteration >= customSlideItem.length) {
+				return;
+			}
+			activeAutoSlide();
+		}, 5000);
+	};
+	activeAutoSlide();
+};
+customAutoSlide();
